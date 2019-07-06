0eebf7d55a7754a1ca43299f4d42150e4ecce55a
Jose Cisneros <jmc@asu.edu>
2019-07-06 13:49:46 -0700

    comp postlist

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,7 +1,21 @@
 import jsonPlaceholder from '../apis/jasonPlaceholder'
+import _ from 'lodash';
+
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+   await dispatch(fetchPosts());  
+
+   const ids= _.map(getState().posts, 'userId');
+   const uniqIds = _.uniq(ids);
+   uniqIds.forEach(id => dispatch(fetchUser(id)));
+};
 
 export const fetchPosts = () => async  dispatch => {
     const response = await jsonPlaceholder.get('/posts');
-    dispatch({type: 'FETCH_POSTS', payload: response})
+    dispatch({ type: 'FETCH_POSTS', payload: response.data })
 };
 
+export const fetchUser = (id) => async dispatch => {
+    const response = await jsonPlaceholder.get('/users/' + id);
+
+    dispatch({type: 'FETCH_USER', payload: response.data});
+};
